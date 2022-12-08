dc365dad590099618e2b1dd6cced2589076eadf7
minjeongit0925 <minjeongit0925@gmail.com>
2022-12-08 11:35:58 +0900

    예제_N개의 수

--- 알고리즘강의/예제_N개의 수.py
+++ 알고리즘강의/예제_N개의 수.py
@@ -1,6 +1,6 @@
 # N개의 수를 입력 받아서 두 수를 뽑아 합이 가장 클 때는?
 """
-1. 정렬로 푼 방법 -> 시간 복잡도 O(NlogN)
+    1. 정렬로 푼 방법 -> 시간 복잡도 O(NlogN)
 """
 num = int(input())
 nums = []
@@ -15,19 +15,21 @@ print(numsSort[-1]+numsSort[-2])
 
 
 """
-2. 완전 탐색으로 푼 방법 -> 시간 복잡도 O(N^2)
-즉, 시간 복잡도가 너무 커서 시간제한이 1초로 주어질 경우에는 실패함. N이 1,000,000까지일 경우
 
-num = int(input())
-nums = []
-sumNum = []
+    2. 완전 탐색으로 푼 방법 -> 시간 복잡도 O(N^2) * 시간 초과로 실패한 경우
+       즉, 시간 복잡도가 너무 커서 시간제한이 1초로 주어질 경우에는 실패함. N이 1,000,000까지일 경우
 
-for i in range(num):
-    nums.append(int(input()))
+        num = int(input())
+        nums = []
+        sumNum = []
 
-for i in range(len(nums)):
-    for j in range(1, len(nums)):
-        sumNum.append(nums[i]+nums[j])
+        for i in range(num):
+            nums.append(int(input()))
 
-print(max(sumNum))
+        for i in range(len(nums)):
+            for j in range(1, len(nums)):
+                sumNum.append(nums[i]+nums[j])
+
+        print(max(sumNum))
+
 """
