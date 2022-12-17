5400f559c8d89decafd34f30095edb5173fd2b5f
minjeongit0925 <minjeongit0925@gmail.com>
2022-12-17 17:27:35 +0900

    1181_단어정렬

--- solved.ac_class2/1181_단어정렬.py
+++ solved.ac_class2/1181_단어정렬.py
@@ -1,4 +1,33 @@
 # 문제
 '''
-    
+    알파벳 소문자로 이루어진 N개의 단어
+    1. 길이가 짧은 것부터
+    2. 길이가 같으면 사전순으로
 '''
+
+# 입력
+'''
+    단어의 개수 N개
+    한줄에 단어 하나씩
+'''
+
+# 출력
+'''
+    조건에 따라 단어 정렬
+    같은건 한번만 출력
+'''
+
+N = int(input())
+
+words = []
+
+for _ in range(N):
+    words.append(input())
+
+words.sort(key=len)
+
+for i in range(len(words)):
+    if words[i] == words[i+1]:
+        words.remove(words[i+1])
+
+print(words)
