f437f3c7c87e8c1e60a4c88b1f18eaac062a1b36
minjeongit0925 <minjeongit0925@gmail.com>
2022-12-18 18:46:49 +0900

    11724_dfs시도

--- 알고리즘강의/DFS_BFS_백트래킹/11724_연결요소의개수.py
+++ 알고리즘강의/DFS_BFS_백트래킹/11724_연결요소의개수.py
@@ -6,7 +6,7 @@
 # 입력
 '''
     정점의 개수 N, 간선의 개수 M
-    양 끝점 u, v
+    양 끝점 u, v  
 '''
 
 # 출력
@@ -14,7 +14,23 @@
     연결 요소의 개수
 '''
 
+from collections import deque
+
 N, M = map(int, input().split())
+adj = [[0] * N for _ in range(N)]
+
 for _ in range(M):
     u, v = map(int, input().split())
+    adj[u][v] = 1
 
+def bfs():
+    dq = deque()
+    dq.append(0)
+    while dq:
+        now = dq.popleft()
+        for nxt in range(N):
+            if adj[now][nxt]:
+                dq.append(nxt)
+    print(dq)
+
+bfs()
