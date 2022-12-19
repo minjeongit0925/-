b0231fda394e661a6dadc1fb9743fddd6bf97f3b
minjeongit0925 <minjeongit0925@gmail.com>
2022-12-19 21:55:03 +0900

    1920_두번째 시도

--- solved.ac_class2/1920_수찾기.py
+++ solved.ac_class2/1920_수찾기.py
@@ -32,3 +32,20 @@
         else:
             print(0)
 """
+
+# 두번째 시도
+
+import sys
+
+N = int(sys.stdin.readline())
+A = set(list(map(int, sys.stdin.readline().split())))
+
+M = int(sys.stdin.readline())
+B = list(map(int, sys.stdin.readline().split()))
+
+for i in B:
+    if i in A:
+        print(1)
+    else:
+        print(0)
+
