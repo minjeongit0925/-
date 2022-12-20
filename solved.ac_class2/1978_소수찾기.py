1af00f71c80457c495b10524fdbf6f944b89d3b0
minjeongit0925 <minjeongit0925@gmail.com>
2022-12-20 21:24:04 +0900

    1978_소수찾기

--- solved.ac_class2/1978_소수찾기.py
+++ solved.ac_class2/1978_소수찾기.py
@@ -16,4 +16,16 @@
 
 N = int(input())
 nums = list(map(int, input().split()))
+answer = 0
 
+# 소수 -> 약수가 1과 자기자신뿐인 수
+for num in nums:
+    nonAns = 0
+    if num > 1: # 숫자가 1보다는 커야함.
+        for i in range(2, num): # 2부터 자기자신-1까지 나누기 시작함.
+            if num % i == 0: # 나머지가 0 이면 약수가 1과 자기자신 외에 있는 것
+                nonAns += 1 # 답이 아님.
+        if nonAns == 0: # 몫이 없는 경우 이 숫자는 소수임.
+            answer += 1
+
+print(answer)
