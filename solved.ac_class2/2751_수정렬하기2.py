85085f22c729fd643deb1edc57c696782f49b263
minjeongit0925 <minjeongit0925@gmail.com>
2022-12-21 22:21:16 +0900

    2751_수정렬하기2

--- solved.ac_class2/2751_수정렬하기2.py
+++ solved.ac_class2/2751_수정렬하기2.py
@@ -13,9 +13,14 @@
 '''
     오름차순으로 정렬
 '''
+import sys
 
-N = int(input())
+N = int(sys.stdin.readline())
 nums = []
 for _ in range(N):
-    nums.append(int(input()))
+    nums.append(int(sys.stdin.readline()))
 
+nums.sort()
+
+for num in nums:
+    print(num)
