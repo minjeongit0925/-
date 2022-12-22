8b1008f27f15202d1f1001ef16ef7c370447a8f2
minjeongit0925 <minjeongit0925@gmail.com>
2022-12-22 21:01:43 +0900

    10814_나이순정렬

--- solved.ac_class2/10814_나이순정렬.py
+++ solved.ac_class2/10814_나이순정렬.py
@@ -20,9 +20,20 @@ N = int(input())
 user = []
 for i in range(N):
     age, name = map(str, input().split())
-    user.append([int(age), name])
+    user.append([int(age), name]) # 이차원 배열 만들기
 
 user.sort(key=lambda x:x[0])
 
 for i in range(len(user)):
     print(user[i][0], user[i][1])
+
+
+# 중요하게 가져가야 할 부분
+'''
+    user.sort(key=lambda x:x[0])
+    이차원 배열일 경우, 나이를 기준으로 이 배열을 정렬하고 싶다면
+    정렬하고자 하는 요소를 설정할 수 있다.
+
+    lambda x:x[0]을 활용해서 이차원 배열의 0번째 요소인 '나이'를 활용해서
+    이차원 배열을 정렬할 수 있다.
+'''
