937622974cb2f62d825a013ee0b18c7a2dd5ed7b
minjeongit0925 <minjeongit0925@gmail.com>
2022-12-29 18:35:50 +0900

    1436_영화감독슘

--- solved.ac_class2/1436_영화감독숌.py
+++ solved.ac_class2/1436_영화감독숌.py
@@ -13,3 +13,22 @@
 '''
     N번째 영화의 제목에 들어간 수
 '''
+
+import sys
+
+N = int(sys.stdin.readline())
+movie = 666
+
+while N != 0:
+    if '666' in str(movie):
+        N = N-1
+        if N == 0:
+            break
+
+    movie = movie + 1
+
+print(movie)
+
+# 숫자를 하나씩 늘려가다가 666이 포함된 숫자가 나오면
+# 카운트를 하나 내린다.
+# 카운트가 0이 되면 그 숫자를 출력한다.
