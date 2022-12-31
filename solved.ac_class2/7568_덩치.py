8c9d9a790267bedee5247bae58ca1610b85b30fe
minjeongit0925 <minjeongit0925@gmail.com>
2022-12-31 21:39:02 +0900

    7568_덩치

--- solved.ac_class2/7568_덩치.py
+++ solved.ac_class2/7568_덩치.py
@@ -15,3 +15,26 @@
 '''
     덩치 등수 첫줄에 순서대로
 '''
+# x 비교 / y 비교 
+# -> 둘다 크면 덩치 큼.
+# -> 둘중에 하나만 크면 동점.
+
+N = int(input())
+big = []
+number = []
+cnt = 0
+
+for _ in range(N):
+    x, y = map(int, input().split())
+    big.append([x, y])
+
+for i in range(N):
+    cnt = 0
+    for j in range(N):
+        if big[i][0] < big[j][0] and big[i][1] < big[j][1]:
+            cnt += 1
+    
+    number.append(cnt + 1)
+
+for i in number:
+    print(i, end=" ")
