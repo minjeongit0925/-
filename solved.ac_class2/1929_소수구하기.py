d239ad9b5206a097dd52fdfc9ad51879a035a96c
minjeongit0925 <minjeongit0925@gmail.com>
2023-01-04 14:45:21 +0900

    1929_입력

--- solved.ac_class2/1929_소수구하기.py
+++ solved.ac_class2/1929_소수구하기.py
@@ -13,3 +13,26 @@
     한줄에 하나씩, 증가하는 순서대로 소수를 출력한다.
 '''
 
+import sys
+
+M, N = map(int, sys.stdin.readline().split())
+nums = []
+while True:
+    if M > N:
+        break
+    for i in range(N-M+1):
+        nums.append(M)
+        M += 1
+print(nums)
+answer = []
+
+'''
+for num in nums:
+    nonAns = 0
+    if num > 1: # 숫자가 1보다는 커야함.
+        for i in range(2, num): # 2부터 자기자신-1까지 나누기 시작함.
+            if num % i == 0: # 나머지가 0 이면 약수가 1과 자기자신 외에 있는 것
+                nonAns += 1 # 답이 아님.
+        if nonAns == 0: # 몫이 없는 경우 이 숫자는 소수임.
+            answer.append(num)
+'''
