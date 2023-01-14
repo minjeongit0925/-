eab43bcd2cf121ed815b86fed44c76ee6de9aa8a
minjeongit0925 <minjeongit0925@gmail.com>
2023-01-14 21:22:33 +0900

    2805_나무자르기

--- solved.ac_class2/2805_나무자르기.py
+++ solved.ac_class2/2805_나무자르기.py
@@ -21,18 +21,18 @@ import sys
 N, M = map(int, sys.stdin.readline().split())
 trees = list(map(int, sys.stdin.readline().split()))
 
-start = 1
-end = sum(trees)
+start = 1 # 시작
+end = sum(trees) # 끝
 
-while start <= end:
-    mid = (start + end) // 2
-    cnt = 0
+while start <= end: # 시작이 끝보다 작을 때까지
+    mid = (start + end) // 2 # 중간값
+    cnt = 0 # 나무의 길이 판단
 
-    for tree in trees:
-        if tree > mid:
-            cnt += tree - mid
-    if cnt >= M:
-        start = mid + 1
+    for tree in trees: # trees 배열에서
+        if tree > mid: # 중간값보다 크면
+            cnt += tree - mid # 나무의 길이에 더하기
+    if cnt >= M: # 원하는 나무의 길이보다 크면
+        start = mid + 1 # 시작에 중간값 + 1 해서 다시 시작
     else:
-        end = mid - 1
+        end = mid - 1 # 그 외에는 끝에서 중간값 -1 해서 다시 시작
 print(end)
