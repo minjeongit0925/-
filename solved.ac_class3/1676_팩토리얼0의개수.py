7f82afbe1e63d4d7142c62ce279186415ea402a3
minjeongit0925 <minjeongit0925@gmail.com>
2023-01-17 18:55:52 +0900

    1676_두번째시도_성공

--- solved.ac_class3/1676_팩토리얼0의개수.py
+++ solved.ac_class3/1676_팩토리얼0의개수.py
@@ -17,15 +17,32 @@ import sys
 
 N = int(sys.stdin.readline())
 factorial = 1
+num = []
+cnt = 0
 
 for i in range(1, N+1):
     factorial = factorial * i
 
+for i in str(factorial):
+    num.append(i)
+
+num.reverse()
+
+for i in range(len(num)):
+    if num[i] == '0':
+        cnt += 1
+    else:
+        break
+
+print(cnt)
+
 # 5의 배수 10의 배수 1개, 25의 배수 10의 배수 2개, 125의 배수 10의 배수 3개
 # 5의 개수 먼저 세고, 25의 개수, 125의 개수를 센다.
 
-count_5 = N // 5
-count_25 = N // 25
-count_125 = N // 125
+'''
+    count_5 = N // 5
+    count_25 = N // 25
+    count_125 = N // 125
 
-print(count_5 + count_25 + count_125)
+    print(count_5 + count_25 + count_125)
+'''
