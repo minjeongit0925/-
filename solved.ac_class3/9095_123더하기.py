b0c416385970cfcd5adcb51b2feefe55ce834ce7
minjeongit0925 <minjeongit0925@gmail.com>
2023-02-22 21:33:11 +0900

    9095_123더하기

--- solved.ac_class3/9095_123더하기.py
+++ solved.ac_class3/9095_123더하기.py
@@ -18,17 +18,17 @@
 import sys
 input = sys.stdin.readline
 
-cache = [0] * 11
-cache[1] = 1
-cache[2] = 2
-cache[3] = 4
+temp = [0] * 11
+temp[1] = 1
+temp[2] = 2
+temp[3] = 4
 
 for i in range(4, 11):
-    cache[i] = sum(cache[i-3:i])
+    temp[i] = sum(temp[i-3:i])
 
 T = int(input())
 for _ in range(T):
-    print(cache[int(input())])
+    print(temp[int(input())])
 
 # n을 1, 2, 3 의 합으로 나타내는 방법
 # ex) 6 = 1 + 2 + 3, 1 * 6, 2 + 2 + 2, 3 + 3, 1 * 3 + 3, 1 * 4 + 2 => 6개
